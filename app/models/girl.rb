4ad27128eb48911781de78c02e90adee01ac0453
ahyataro <gmwnk183@gmail.com>
2017-05-06 23:58:20 +0900

    テンションの上がる編集画面を実装

--- app/models/girl.rb
+++ app/models/girl.rb
@@ -1,3 +1,4 @@
 class Girl < ApplicationRecord
   has_many :images, dependent: :destroy, foreign_key: 'girl_id'
+  has_one :thumbnail_image, class_name: 'Image', primary_key: 'thumbnail_image_id', foreign_key: 'id'
 end

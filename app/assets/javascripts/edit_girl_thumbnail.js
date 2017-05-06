d427946380b1b2f3501eacb3d973ff20154376c4
ahyataro <gmwnk183@gmail.com>
2017-05-07 00:43:25 +0900

    リファクタリング

--- app/assets/javascripts/edit_girl_thumbnail.js
+++ app/assets/javascripts/edit_girl_thumbnail.js
@@ -1,8 +1,8 @@
 $('.thumbnail-candidate-img').on('click', function(){
-    $('.thumbnail-id').val($(this).attr('id'))
+    $('#thumbnail-id').val($(this).attr('id'))
     $('.thumbnail-img').attr('src', $(this).attr('src'))
 })
 
-$('.open-thumbnail-candidate').on('click', function(){
+$('#open-thumbnail-candidate').on('click', function(){
    $('.thumbnail-candidate-panel').slideToggle()
 })

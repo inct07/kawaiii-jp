4f8a2c06bea4adddc221d609015e643c3143d0d0
rasayama <nanor610@gmail.com>
2017-05-07 18:27:01 +0900

    たまらんボタン修正

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -1,19 +1,25 @@
-$('.favorite-button').on('click', function(){
-    favorite($(this).val(),$(this))
+$('.favorite-button').on('click', function() {
+  favorite($(this).val(),$(this))
 })
 
 function favorite(id, button) {
-    if (button.hasClass('favorite')){
-        $.ajax({url: '/favorite', type: 'POST', data: {_method: 'delete', authenticity_token: $('#authenticity_token').val(), image_id: id}})
-            .done(function(data){
-                button.removeClass('favorite').addClass('no-favorite')
-                button.removeClass('btn-danger').addClass('btn-default')
-            })
-    }else{
-        $.ajax({url: '/favorite', type: 'POST', data: {_method: 'post', authenticity_token: $('#authenticity_token').val(), image_id: id}})
-            .done(function(data){
-                button.removeClass('no-favorite').addClass('favorite')
-                button.removeClass('btn-default').addClass('btn-danger')
-            })
-    }
+  if (button.hasClass('favorite')) {
+    $.ajax({url: '/favorite', type: 'POST', data: {_method: 'delete',
+      authenticity_token: $('#authenticity_token').val(), image_id: id}})
+        .done(function(data){
+            button.removeClass('favorite').addClass('no-favorite')
+            button.addClass('btn-default')
+            button.css("background", "white")
+            button.children().removeClass('glyphicon-heart').addClass('glyphicon-heart-empty')
+        })
+  } else {
+    $.ajax({url: '/favorite', type: 'POST', data: {_method: 'post',
+      authenticity_token: $('#authenticity_token').val(), image_id: id}})
+        .done(function(data){
+            button.removeClass('no-favorite').addClass('favorite')
+            button.removeClass('btn-default')
+            button.css("background", "#ff69b4")
+            button.children().removeClass('glyphicon-heart-empty').addClass('glyphicon-heart')
+        })
+  }
 }
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w(favorite.js edit_girl_thumbnail.js)
-Rails.application.config.assets.precompile += %w(thumbnail.css.scss girl_img.css.scss)
+Rails.application.config.assets.precompile += %w(thumbnail.css.scss girl_image.css.scss)

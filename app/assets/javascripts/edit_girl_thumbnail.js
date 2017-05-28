039ced4fb9f933956037182cedd7f0d913f55362
rasayama <nanor610@gmail.com>
2017-05-28 18:54:34 +0900

    サムネ画像まるまる

--- app/assets/javascripts/edit_girl_thumbnail.js
+++ app/assets/javascripts/edit_girl_thumbnail.js
@@ -1,6 +1,6 @@
 $('.thumbnail-candidate-img').on('click', function(){
     $('#thumbnail-id').val($(this).attr('id'))
-    $('.thumbnail-img').attr('src', $(this).attr('src'))
+    $('.thumbnail-img').attr('style', $(this).attr('style'))
 })
 
 $('#open-thumbnail-candidate').on('click', function(){

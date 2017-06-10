deeb2bff7249a0209bca9e9d701b2363de720688
ahyataro <gmwnk183@gmail.com>
2017-06-10 19:48:36 +0900

    自分がたまらんしたかどうかを判定するときのイケてないロジックを修正

--- app/controllers/mypage/favorite_images_controller.rb
+++ app/controllers/mypage/favorite_images_controller.rb
@@ -1,16 +1,18 @@
 class Mypage::FavoriteImagesController < ApplicationController
   def index
-    all_favorite_images = current_user.favorite_images.includes(:favorited_users, :girl)
+    all_favorite_images = current_user.favorite_images.includes(:girl)
     render 'mypage/favorite_images/no_favorite_images' if all_favorite_images.blank?
     @all_girls = all_favorite_images.map(&:girl).uniq
     @favorite_images = all_favorite_images.page(params[:page]).per(PAGE_LIMIT)
+    @current_user_favorite_images = current_user.favorite_images
   end
 
   def show
     @girl_id = params[:id]
     all_favorite_images = current_user.favorite_images
     @all_girls = all_favorite_images.map(&:girl).uniq
-    @favorite_images = all_favorite_images.where(girl_id: @girl_id).includes(:favorited_users, :girl)
+    @favorite_images = all_favorite_images.where(girl_id: @girl_id).includes(:girl)
     @favorite_images = @favorite_images.page(params[:page]).per(PAGE_LIMIT)
+    @current_user_favorite_images = current_user.favorite_images.where(girl_id: @girl_id)
   end
 end
